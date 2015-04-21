b1e800eb6b13dd30375801b2ef8f817f5fdd5def
jrwebb56 <jrwebb56@gmail.com>
2015-04-20 19:35:40 -0700

    File association to open file in program

---
+++ dscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DIRECTORY=~/desire/SYSPIC.lst
+echo "/NEW" > $DIRECTORY
+echo -n "/PIC '" >> $DIRECTORY
+echo "$1'" >> $DIRECTORY
+echo "/" >> $DIRECTORY
+cat $1 >> $DIRECTORY
+echo "/" >> $DIRECTORY
